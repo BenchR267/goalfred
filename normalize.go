b6c979d951e81f6bfc03226868ab3566e78f487f
Benjamin Herzog <benjamin.herzog@lovoo.com>
2016-09-14 11:19:57 +0200

    Test setting the encoding to UTF-8 instead of UTF8-MAC

--- normalize.go
+++ normalize.go
@@ -8,7 +8,7 @@ import (
 // Normalize fixes problems with string encoding regarding the usage of special characters in Alfred.
 // For more info on this topic, please refer to this thread: http://www.alfredforum.com/topic/2015-encoding-issue/
 func Normalize(input string) (output string, err error) {
-	iconv := exec.Command("iconv", "-f", "UTF8-MAC")
+	iconv := exec.Command("iconv", "-f", "UTF-8")
 	iconvIn, err := iconv.StdinPipe()
 	iconvOut, err := iconv.StdoutPipe()
 
